dd9a41e8f0ab56910511abebc32753c856fd6c73
Light <Light010191@yandex.ru>
2023-05-12 01:53:29 +0300

    add project

---
+++ api/GPTSend.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace api
+{
+    public partial class GptSend
+    {
+        [JsonPropertyName("model")]
+        public string Model { get; set; }
+
+        [JsonPropertyName("messages")]
+        public Message[] Messages { get; set; }       
+    }
+
+    public partial class Message
+    {
+        [JsonPropertyName("role")]
+        public string Role { get; set; }
+
+        [JsonPropertyName("content")]
+        public string Content { get; set; }
+    }
+}
---
+++ api/Program.cs
@@ -0,0 +1,24 @@
+﻿using api;
+using ChatGPTApi;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+
+var openAiKey =Environment.GetEnvironmentVariable("OpenAI_token");
+string userMassage="";
+Console.WriteLine("Введите Ваш Вопрос или нажмите 0 для выхода : ");
+while (true)
+{
+    userMassage = Console.ReadLine();
+    if (userMassage != "0" && !string.IsNullOrEmpty(userMassage))
+    {
+        ChatGptClient chatGptClient = new ChatGptClient(openAiKey);
+        
+        Console.WriteLine(await chatGptClient.GetChatGptMessage(userMassage));            
+    }
+    else
+    {
+        break;
+    }    
+}
